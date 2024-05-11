1461f36a757aefddfaf81b7d69e70b6b3a79f641
mzain01 <150827453+mzain01@users.noreply.github.com>
2024-05-11 20:49:27 +0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,142 @@
+<?php
+include('header.php');
+?>
+<section class="breadcrumbs">
+<div class="breadcrumb-sm-images">
+<div class="inner-banner-1 magnetic-item">
+<img src="assets/img/inner-pages/inner-banner-1.png" alt>
+</div>
+<div class="inner-banner-2 magnetic-item">
+<img src="assets/img/inner-pages/inner-banner-2.png" alt>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-12">
+<div class="breadcrumb-wrapper">
+<div class="breadcrumb-cnt">
+<span>Contact</span>
+<h1>"For Any Querry"</h1>
+<div class="breadcrumb-list">
+<a href="index.php">Home</a><img src="assets/img/inner-pages/breadcrumb-arrow.svg" alt> Contact
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<div class="contact-page-wrap sec-mar">
+<div class="container">
+<div class="row g-lg-4 gy-5">
+<div class="col-lg-6">
+<div class="contact-content">
+<span>CONTACT WITH US</span>
+<h2>LET’S WORK TOGETHER?</h2>
+<p>I have worls-class, flexible support via live chat, email and hone. I guarantee that you’ll be able to have any issue resolved within 24 hours.</p>
+<div class="informations">
+<div class="single-info">
+<div class="icon">
+<i class="fas fa-map-marker-alt"></i>
+</div>
+<div class="info">
+<p>168/170, Avenue 01, Old York <br> Drive Rich Mirpur DOHS, Bangladesh</p>
+</div>
+</div>
+<div class="single-info">
+<div class="icon">
+<i class="fas fa-phone-alt"></i>
+</div>
+<div class="info">
+<a href="tel:05661111985">+880 566 1111 985</a>
+<a href="tel:06571111576">+880 657 1111 576</a>
+</div>
+</div>
+<div class="single-info">
+<div class="icon">
+<i class="far fa-envelope"></i>
+</div>
+<div class="info">
+<a href="https://demo-egenslab.b-cdn.net/cdn-cgi/l/email-protection#fada93949c95ba9f829b978a969fd4999597"><span class="__cf_email__" data-cfemail="b9d0d7dfd6f9dcc1d8d4c9d5dc97dad6d4">[email&#160;protected]</span></a>
+<a href="https://demo-egenslab.b-cdn.net/cdn-cgi/l/email-protection#efcf86818980af9c9a9f9f809d9bc18c8082"><span class="__cf_email__" data-cfemail="c4adaaa2ab84b7b1b4b4abb6b0eaa7aba9">[email&#160;protected]</span></a>
+</div>
+</div>
+</div>
+<div class="follow-area">
+<h5 class="blog-widget-title">Follow Us</h5>
+<p class="para">Follow us on Social Network</p>
+<div class="blog-widget-body">
+<ul class="follow-list d-flex flex-row align-items-start gap-4">
+<li><a href="https://www.facebook.com/"><i class="bx bxl-facebook"></i></a></li>
+<li><a href="https://www.twitter.com/"><i class="bx bxl-twitter"></i></a></li>
+<li><a href="https://www.instagram.com/"><i class="bx bxl-instagram"></i></a></li>
+<li><a href="https://www.pinterest.com/"><i class="bx bxl-pinterest"></i></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-6">
+<div class="contact-form-wrap mb-5">
+<div class="form-tltle">
+<h5>Make a Free Consulting</h5>
+</div>
+<div class="contact-form">
+<form>
+<div class="row">
+<div class="col-md-6 mb-20">
+<div class="form-inner">
+<label>first name</label>
+<input type="text">
+</div>
+</div>
+<div class="col-md-6 mb-20">
+<div class="form-inner">
+<label>Last name</label>
+<input type="text">
+</div>
+</div>
+<div class="col-lg-12 mb-20">
+<div class="form-inner">
+<label>Company/Organization</label>
+<input type="text">
+</div>
+</div>
+<div class="col-lg-12 mb-20">
+<div class="form-inner">
+<label>Email</label>
+<input type="email">
+</div>
+</div>
+<div class="col-lg-12 mb-20">
+<div class="form-inner">
+<label>Phone</label>
+<input type="email">
+</div>
+</div>
+<div class="col-lg-12 mb-20">
+<div class="form-inner">
+<label>Message</label>
+<textarea></textarea>
+</div>
+</div>
+<div class="col-lg-12">
+<div class="form-inner">
+<button class="primary-btn3" type="submit">Submit</button>
+</div>
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+<!-- </div>
+<div class="contact-map">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3649.564763185785!2d90.36311167608078!3d23.834071185557615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c14c8682a473%3A0xa6c74743d52adb88!2sEgens%20Lab!5e0!3m2!1sen!2sbd!4v1685535738307!5m2!1sen!2sbd" style="border:0;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div> -->
+<?php
+include('footer.php');
+?>
